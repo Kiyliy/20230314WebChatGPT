8d4b497d2bbea2afa6a19df0e16f813b116b7f1a
Kiyliy <122872761+Kiyliy@users.noreply.github.com>
2023-04-15 18:05:10 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,7 +16,7 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'K-Render',
+      name: 'kimi',
       description: '世界碎了，但潮汐在心中 </a>',
     },
   }
